971c6e1d22806ba1d50e772d2bce286b7dac43e7
kyuminn <starkyumin0622@gmail.com>
2023-01-13 11:56:05 +0900

    HandlerAdapter 인터페이스 추가

---
+++ src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.frontcontroller.v5;
+
+import hello.servlet.web.frontcontroller.ModelView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+
+    // handler = controller라고 생각할 것
+    // supports()= 파라미터로 넘어온 controller를 처리할 adapter가 있는지 검사하는 메서드
+    boolean supports(Object handler);
+
+    ModelView handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
